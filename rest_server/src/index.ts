b14f8d1d14be52060280a2060ddce5b0e85c9d73
Sebastian-Boettcher <84092041+Account-erstellen@users.noreply.github.com>
2025-01-28 17:07:59 +0100

    Added Hono

---
+++ rest_server/src/index.ts
@@ -0,0 +1,25 @@
+import { Hono } from 'hono'
+//import { sql } from 'bun'
+
+const app = new Hono()
+
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+// Will match `/api/animal` and `/api/animal/:type`
+app.get('/api/animal/:type?', (c) => c.text('Animal!'))
+
+app.get('/posts/:id/comment/:comment_id', async (c) => {
+  const { id, comment_id } = c.req.param()
+  // Datenbank eintrag erstellen
+  return c.text('Wurde erfolgreich hinzugefügt mit ID: ' + id + ' Und dem Kommentar ' + comment_id)
+})
+
+app.notFound((c) =>{
+  return c.text('Falsch abgebogen!')
+})
+
+
+export default app
